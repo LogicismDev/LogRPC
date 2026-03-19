e7856590d937cbc9ce81ee1680ee4fbea3f2ffc4
LogicismDev <blazewalker462@protonmail.com>
2026-03-19 02:20:09 -0700

    Fix and Add Nitter Scraping to X.com

--- src/me/Logicism/LogRPC/presence/website/TwitterPresence.java
+++ src/me/Logicism/LogRPC/presence/website/TwitterPresence.java
@@ -1,19 +1,54 @@
 package me.Logicism.LogRPC.presence.website;
 
 import com.jagrosh.discordipc.entities.ActivityType;
+import me.Logicism.LogRPC.LogRPC;
 import me.Logicism.LogRPC.core.data.BrowserHTMLData;
 import me.Logicism.LogRPC.core.data.PresenceData;
+import me.Logicism.LogRPC.network.BrowserClient;
+import me.Logicism.LogRPC.network.BrowserData;
 import me.Logicism.LogRPC.presence.Presence;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Set;
+import java.io.IOException;
+import java.net.URL;
+import java.util.*;
 
 public class TwitterPresence extends Presence {
 
+    private String nitterDisplayName;
+    private String nitterUsername;
+
     public TwitterPresence(PresenceData data) {
         super(802958757909889054L, data);
+
+        try {
+            if (LogRPC.INSTANCE.getConfig().isNitterScraperEnabled()) {
+                BrowserHTMLData bData = (BrowserHTMLData) this.data;
+                URL url = new URL(bData.getURL());
+
+                if (url.getHost().equals("x.com")) {
+                    Map<String, String> headers = new HashMap<>();
+
+                    if (!bData.getURL().startsWith("https://x.com/i/") && !bData.getURL().startsWith("https://x.com/explore/") && !bData.getURL().startsWith("https://x.com/notifications/")) {
+                        BrowserData bd = BrowserClient.executeGETRequest(new URL(LogRPC.INSTANCE.getConfig().getNitterScraperInstance() + bData.getURL().substring("https://x.com/".length())), headers);
+                        Document document = Jsoup.parse(BrowserClient.requestToString(bd.getResponse()));
+
+                        nitterDisplayName = document.getElementsByClass("profile-card-fullname").text();
+                        nitterUsername = document.getElementsByClass("profile-card-username").text();
+                    } else {
+                        BrowserData bd = BrowserClient.executeGETRequest(new URL(LogRPC.INSTANCE.getConfig().getNitterScraperInstance() + bData.getURL().substring("https://x.com/".length())), headers);
+                        Document document = Jsoup.parse(BrowserClient.requestToString(bd.getResponse()));
+
+                        nitterDisplayName = document.getElementsByClass("fullname").text();
+                        nitterUsername = document.getElementsByClass("username").text();
+                    }
+                }
+            }
+        } catch (IOException e) {
+
+        }
     }
 
     @Override
@@ -31,8 +66,8 @@ public class TwitterPresence extends Presence {
             return "Browsing Explore";
         } else if (data.getURL().endsWith("https://x.com/notifications") || data.getURL().endsWith("https://x.com/notifications/verified") || data.getURL().endsWith("https://x.com/notifications/mentions")) {
             return "Browsing Notifications";
-        } else if (data.getURL().startsWith("https://x.com/messages/") || data.getURL().equals("https://x.com/messages")) {
-            return "Reading DMs";
+        } else if (data.getURL().startsWith("https://x.com/i/chat/") || data.getURL().equals("https://x.com/i/chat/")) {
+            return "Reading Chats";
         } else if (data.getURL().startsWith("https://x.com/i/bookmarks")) {
             return "Browsing Bookmarks";
         } else if (data.getURL().endsWith("/lists") || data.getURL().startsWith("https://x.com/i/lists/")) {
@@ -54,9 +89,13 @@ public class TwitterPresence extends Presence {
         } else if (data.getURL().contains("/communities/")) {
             return "Browsing Communities";
         } else {
-            Element userElement = data.getHTMLDocument().selectXpath("/html/body/div[1]/div/div/div[2]/main/div/div/div/div/div/div[3]/div/div/div/div/div[2]/div/div/div/div[2]/div/div/div/span").first();
-            if (userElement != null) {
+            if (LogRPC.INSTANCE.getConfig().isNitterScraperEnabled()) {
                 return "Viewing X.com User";
+            } else {
+                Element userNameElement = data.getHTMLDocument().select("#react-root > div > div > div.css-175oi2r.r-1f2l425.r-13qz1uu.r-417010.r-18u37iz > main > div > div > div > div.css-175oi2r.r-kemksi.r-1kqtdi0.r-1ua6aaf.r-th6na.r-1phboty.r-16y2uox.r-184en5c.r-1abdc3e.r-1lg4w6u.r-f8sm7e.r-13qz1uu.r-1ye8kvj > div > div:nth-child(3) > div > div > div:nth-child(1) > div > div.css-175oi2r.r-18u37iz.r-1w6e6rj.r-6gpygo.r-14gqq1x > div.css-175oi2r.r-eqz5dr.r-1wbh5a2.r-1wron08 > div > div > div.css-175oi2r.r-1awozwy.r-18u37iz.r-1wbh5a2 > div > div > div > span").first();
+                if (userNameElement != null) {
+                    return "Viewing X.com User";
+                }
             }
         }
 
@@ -83,37 +122,39 @@ public class TwitterPresence extends Presence {
             return "Verified";
         } else if (data.getURL().equals("https://x.com/notifications/mentions")) {
             return "Mentions";
-        } else if (data.getURL().startsWith("https://x.com/messages/")) {
-            if (data.getURL().equals("https://x.com/messages/settings")) {
+        } else if (data.getURL().startsWith("https://x.com/i/chat")) {
+            if (data.getURL().equals("https://x.com/i/chat/settings")) {
                 return "Settings";
-            } else if (data.getURL().equals("https://x.com/messages/compose")) {
-                return "Composing New Message";
             } else {
-                Element displayNameElement = data.getHTMLDocument().selectXpath("/html/body/div[1]/div/div/div[2]/main/div/div/div/div[2]/div/div/div/div/div[1]/div/div/div[1]/div/div[2]/div/div[1]/a/div/div[1]/span/span[1]").first();
-                if (displayNameElement != null) {
-                    Element userNameElement = data.getHTMLDocument().selectXpath("/html/body/div[1]/div/div/div[2]/main/div/div/div/div[2]/div/div/div/div/div[1]/div/div/div[1]/div/div[2]/div/div[2]/div/a/div/div/span").first();
+                Element displayNameElement = data.getHTMLDocument().selectXpath("/html/body/div[1]/div/div/div[2]/main/div/div/div/div/div/div/div/div[2]/div/div/div[1]/div[2]/div[2]/div/div[1]").first();
+                return displayNameElement.text();
+            }
+        } else if (data.getURL().contains("/status/")) {
+            if (LogRPC.INSTANCE.getConfig().isNitterScraperEnabled()) {
+                return nitterDisplayName + " (" + nitterUsername + ")";
+            } else {
+                if (data.getURL().contains("/photo/")) {
+                    Element displayNameElement = data.getHTMLDocument().selectXpath("/html/body/div[1]/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div[2]/section/div/div/div[1]/div/div/article/div/div/div[2]/div[2]/div/div/div[1]/div/div/div[1]/div/a/div/div[1]/span/span").first();
+                    Element userNameElement = data.getHTMLDocument().selectXpath("/html/body/div[1]/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div[2]/section/div/div/div[1]/div/div/article/div/div/div[2]/div[2]/div/div/div[1]/div/div/div[2]/div/div/a/div/span").first();
+
+                    return displayNameElement.text() + " (" + userNameElement.text() + ")";
+                } else {
+                    Element displayNameElement = data.getHTMLDocument().selectXpath("/html/body/div[1]/div/div/div[2]/main/div/div/div/div[1]/div/section/div/div/div[1]/div/div/article/div/div/div[2]/div[2]/div/div/div[1]/div/div/div[1]/div/a/div/div[1]/span/span").first();
+                    Element userNameElement = data.getHTMLDocument().selectXpath("/html/body/div[1]/div/div/div[2]/main/div/div/div/div[1]/div/section/div/div/div[1]/div/div/article/div/div/div[2]/div[2]/div/div/div[1]/div/div/div[2]/div/div/a/div/span").first();
 
                     return displayNameElement.text() + " (" + userNameElement.text() + ")";
                 }
             }
-        } else if (data.getURL().contains("/status/")) {
-            if (data.getURL().contains("/photo/")) {
-                Element displayNameElement = data.getHTMLDocument().selectXpath("/html/body/div[1]/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div[2]/section/div/div/div[1]/div[1]/div/article/div/div/div[2]/div[2]/div/div/div[1]/div/div/div[1]/div/a/div/div[1]/span/span").first();
-                Element userNameElement = data.getHTMLDocument().selectXpath("/html/body/div[1]/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div[2]/section/div/div/div[1]/div[1]/div/article/div/div/div[2]/div[2]/div/div/div[1]/div/div/div[2]/div/div/a/div/span").first();
-
-                return displayNameElement.text() + " (" + userNameElement.text() + ")";
-            } else {
-                Element displayNameElement = data.getHTMLDocument().selectXpath("/html/body/div[1]/div/div/div[2]/main/div/div/div/div/div/section/div/div/div[1]/div[1]/div/article/div/div/div[2]/div[2]/div/div/div[1]/div/div/div[1]/div/a/div/div[1]/span/span").first();
-                Element userNameElement = data.getHTMLDocument().selectXpath("/html/body/div[1]/div/div/div[2]/main/div/div/div/div/div/section/div/div/div[1]/div[1]/div/article/div/div/div[2]/div[2]/div/div/div[1]/div/div/div[2]/div/div/a/div/span").first();
-
-                return displayNameElement.text() + " (" + userNameElement.text() + ")";
-            }
         } else {
-            Element displayNameElement = data.getHTMLDocument().selectXpath("/html/body/div[1]/div/div/div[2]/main/div/div/div/div/div/div[3]/div/div/div/div/div[2]/div/div/div/div[1]/div/div/span/span[1]").first();
-            if (displayNameElement != null) {
-                Element userNameElement = data.getHTMLDocument().selectXpath("/html/body/div[1]/div/div/div[2]/main/div/div/div/div/div/div[3]/div/div/div/div/div[2]/div/div/div/div[2]/div/div/div/span").first();
+            if (LogRPC.INSTANCE.getConfig().isNitterScraperEnabled()) {
+                return nitterDisplayName + " (" + nitterUsername + ")";
+            } else {
+                Element displayNameElement = data.getHTMLDocument().selectXpath("/html/body/div[1]/div/div/div[2]/main/div/div/div/div[1]/div/div[3]/div/div/div[1]/div/div[2]/div[1]/div/div/div[1]/div/div/span/span[1]    ").first();
+                if (displayNameElement != null) {
+                    Element userNameElement = data.getHTMLDocument().selectXpath("/html/body/div[1]/div/div/div[2]/main/div/div/div/div[1]/div/div[3]/div/div/div[1]/div/div[2]/div[1]/div/div/div[2]/div/div/div/span").first();
 
-                return displayNameElement.text() + " (" + userNameElement.text() + ")";
+                    return displayNameElement.text() + " (" + userNameElement.text() + ")";
+                }
             }
         }
 
